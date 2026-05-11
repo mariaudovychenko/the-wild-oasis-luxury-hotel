d3eb61b4e778bc8bcfaada131e7f4c2c42a60faa
Maria Udovychenko <m.udovychenko@yahoo.com>
2026-05-11 18:29:29 +0100

    Make cabin detail page dynamic

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,30 +1,35 @@
 import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
 import Spinner from "@/app/_components/Spinner";
+
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 
 import Image from "next/image";
 import { Suspense } from "react";
-
+export const dynamic = "force-dynamic";
 // export const metadata = {
 //   title: "Cabin",
 // };
 
 export async function generateMetadata({ params }) {
-  const { name } = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+  const { name } = await getCabin(cabinId);
+
   return { title: `Cabin ${name}` };
 }
 
-export async function generateStaticParams() {
-  const cabins = await getCabins();
+    // export async function generateStaticParams() {
+    //   const cabins = await getCabins();
 
-  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+    //   const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
 
-  return ids;
-}
+    //   return ids;
+    // }
 
 export default async function Page({ params }) {
-  const cabin = await getCabin(params.cabinId);
+  // const cabin = await getCabin(params.cabinId);
+  const { cabinId } = await params;
+  const cabin = await getCabin(cabinId);
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
