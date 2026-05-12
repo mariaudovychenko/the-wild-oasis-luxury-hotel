90136675b012a97b3abc74b360f88b15906c83c7
Maria Udovychenko <m.udovychenko@yahoo.com>
2026-05-12 15:26:21 +0100

    Patch Next security issue and make cabin page dynamic

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,7 +2,7 @@ import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
 import Spinner from "@/app/_components/Spinner";
 
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { getCabin} from "@/app/_lib/data-service";
 
 import Image from "next/image";
 import { Suspense } from "react";
