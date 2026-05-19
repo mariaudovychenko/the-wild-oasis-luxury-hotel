aa9e9e9ec737bbba569d823aab5a5e3a0745aceb
Maria Udovychenko <m.udovychenko@yahoo.com>
2026-05-19 23:24:06 +0100

    Allow Supabase storage images

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'iomhqpffeazdehrqzmgp.supabase.co',
+            hostname: 'rrnrdvyhinpvjgktkfrkh.supabase.co',
             port: '',
             pathname: '/storage/v1/object/public/cabin-images/**',
             search: '',
